a7835099fd1439709b7958a8cb8cfb7ff80279b0
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2024-04-20 07:53:03 +0300

    Install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# Ensure pip3 is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
 # Install Flask version 2.1.0 using pip3
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
