8d39fc5bc6928ad6e8090b85193367470fc87968
morrisaiden <mwaimorris1997@gmail.com>
2024-06-13 01:14:33 -0700

    reducing error messages

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+reducing the number of failed requests on nginx
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/65535/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
+}
+
+#restarting Nginx
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
