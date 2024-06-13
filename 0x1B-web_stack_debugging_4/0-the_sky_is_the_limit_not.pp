1a960c3738afa1cb783f9546bd24149bc1632c48
morrisaiden <mwaimorris1997@gmail.com>
2024-06-13 01:25:03 -0700

    holberto user config

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 reducing the number of failed requests on nginx
 
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4000/" /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin'
 }
 
